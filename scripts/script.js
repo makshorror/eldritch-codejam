392864a98e8bbde34f28b189102ebf6863417c7c
makshorror <makshorror96@gmail.com>
2022-08-24 01:35:20 +0300

    fix: layouts

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ const ancient = document.querySelectorAll(".ancient");
 const levelBlock = document.querySelector(".level")
 const level = document.querySelectorAll(".levels");
 const shuffle = document.querySelector(".shuffle")
-const stageContainer = document.querySelector(".current-state")
-const cardBack = document.querySelector(".cardBack")
+const stageContainer = document.querySelector(".container-game")
+let cards = document.querySelector(".cards")
 let properties = {
     ancient: 0,
     level: 0
@@ -12,8 +12,7 @@ let properties = {
 
 ancient.forEach(function (img) {
     img.addEventListener("click", function (){
-        let idAncient = this.id.replace(/id/ig, '') - 1;
-        properties.ancient = idAncient;
+        properties.ancient = this.id.replace(/id/ig, '') - 1;
         levelBlock.style.visibility = "visible"
         levelBlock.style.opacity = "100%"
     })
@@ -23,8 +22,7 @@ ancient.forEach(function (img) {
 
 level.forEach(function (li) {
     li.addEventListener("click", function (){
-        let idLevel = this.id.replace(/level/ig, '') - 1;
-        properties.level = idLevel;
+        properties.level = this.id.replace(/level/ig, '') - 1;
         levelBlock.style.visibility = "hidden"
         levelBlock.style.opacity = "0"
         shuffle.style.visibility = "visible"
@@ -32,12 +30,12 @@ level.forEach(function (li) {
     })
 })
 
+
+
 shuffle.addEventListener("click", () => {
     shuffle.style.visibility = "hidden"
     shuffle.style.opacity = "0"
     stageContainer.style.visibility = "visible"
     stageContainer.style.opacity = "100%"
-    cardBack.style.visibility = "visible"
-    cardBack.style.opacity = "100%"
 
 })
