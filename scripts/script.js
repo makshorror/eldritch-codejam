c0b030c95eb3510959bc7f0618dc1373ddb2cd33
makshorror <makshorror96@gmail.com>
2022-08-24 15:36:03 +0300

    fix: layouts

--- scripts/script.js
+++ scripts/script.js
@@ -93,6 +93,82 @@ let properties = {
 
 
 
+//Cards///////////////////////////////////////////////
+let blueCards = [
+    {
+        id: 'blue1',
+        src: './assets/MythicCards/blue/blue1.png',
+        difficulty: 'hard',
+        color:'blue'
+    },
+    // {
+    //     id: 'blue2',
+    //     cardFace: blueCardsAssets.blue2,
+    //     difficulty: 'hard',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue3',
+    //     cardFace: blueCardsAssets.blue3,
+    //     difficulty: 'easy',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue4',
+    //     cardFace: blueCardsAssets.blue4,
+    //     difficulty: 'easy',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue5',
+    //     cardFace: blueCardsAssets.blue5,
+    //     difficulty: 'easy',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue6',
+    //     cardFace: blueCardsAssets.blue6,
+    //     difficulty: 'hard',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue7',
+    //     cardFace: blueCardsAssets.blue7,
+    //     difficulty: 'normal',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue8',
+    //     cardFace: blueCardsAssets.blue8,
+    //     difficulty: 'hard',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue9',
+    //     cardFace: blueCardsAssets.blue9,
+    //     difficulty: 'normal',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue10',
+    //     cardFace: blueCardsAssets.blue10,
+    //     difficulty: 'easy',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue11',
+    //     cardFace: blueCardsAssets.blue11,
+    //     difficulty: 'normal',
+    //     color:'blue'
+    // },
+    // {
+    //     id: 'blue12',
+    //     cardFace: blueCardsAssets.blue12,
+    //     difficulty: 'normal',
+    //     color:'blue'
+    // },
+]
+
 //Change//////////////////////////////////////////////
 ancient.forEach(function (img) {
     img.addEventListener("click", function (){
@@ -174,5 +250,6 @@ function shuffleCard(){
         brownDots[2].innerHTML = ancientShubNiggurath.thirdState.brown
         blueDots[2].innerHTML = ancientShubNiggurath.thirdState.blue
     }
+
 }
 //////////////////////////////////////////////////////
